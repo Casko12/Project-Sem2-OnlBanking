f52b99c53e98b913c4d081d3be2f68b02adc8a49
Hunghh90 <hungbkvn9x1@gmail.com>
2023-03-02 19:27:09 +0700

    hung

---
+++ database/migrations/2023_03_02_062535_create_table_user.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->date("birthday");
+            $table->string("address");
+            $table->string("national_id")->unique();
+            $table->string("image_face");
+            $table->string("image_id1");
+            $table->string("image_id2");
+            $table->boolean("status")->default(true);
+            $table->string("pin")->nullable();
+            $table->string("telephone");
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

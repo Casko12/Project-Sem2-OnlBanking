1f4abff05fd53eb9f103981949a75ce4bedd6429
Admin <tranvanthanh060699@gmail.com>
2023-03-03 00:19:50 +0700

    up layout

--- resources/views/user/trans.blade.php
+++ resources/views/user/trans.blade.php
@@ -1,20 +1,18 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Trang chuyển khoản </title>
-    @include("user.html.css")
-</head>
-<body>
-@include("user.html.header")
-@include("user.html.section-banner")
+@section("custom_css")
+    <link rel="stylesheet" type="text/css" href="/user/html/css/bootstrap.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/elegant-icons.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/all.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/animate.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/nice-select.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/default.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/style.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/responsive.css" media="all" />
+@endsection
+@extends("user.layout")
 
+@section("main_content")
 
-
-<div class="col-md-5">
+<div class="col-md-5" style="margin: 200px">
 <form>
     <div class="mb-3">
         <label  class="form-label">Số Tài Khoản</label>
@@ -39,7 +37,21 @@
     <button type="submit" class="btn btn-primary">Chuyển Khoản</button>
 </form>
 </div>
+@endsection
+@section("custom_js")
+    <script type="text/javascript" src="/user/html/js/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="/user/html/js/preloader.js"></script>
+    <script type="text/javascript" src="/user/html/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="/user/html/js/jquery.smoothscroll.min.js"></script>
+    <script type="text/javascript" src="/user/html/js/jquery.waypoints.min.js"></script>
+    <script type="text/javascript" src="/user/html/js/jquery.counterup.min.js"></script>
+    <script type="text/javascript" src="/user/html/js/jquery.nice-select.min.js"></script>
+    <script type="text/javascript" src="/user/html/js/parallax.js"></script>
+    <script type="text/javascript" src="/user/html/js/jquery.parallax-scroll.js"></script>
+    <script type="text/javascript" src="/user/html/js/wow.min.js"></script>
 
-@include("user.html.js")
-</body>
-</html>
+    <script type="text/javascript" src="/user/html/js/custom.js"></script>
+
+@endsection
+
+

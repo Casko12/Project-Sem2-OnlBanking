c836fe3c0123fe4b3fed4b42d44825950cc3edb1
Administrator <tranvanthanh060699@gmail.com>
2023-03-02 19:57:37 +0700

    up UserController

--- public/user/html/js/preloader.js
+++ public/user/html/js/preloader.js
@@ -1,18 +1,18 @@
 (function ($) {
-  "use strict";
+    "use strict";
 
-  /*============= preloader js css =============*/
-  var cites = [];
-  cites[0] = "Chúng tôi luôn sẵn sàng được phục vụ bạn.";
-  var cite = cites[Math.floor(Math.random() * cites.length)];
-  $("#preloader p").text(cite);
-  $("#preloader").addClass("loading");
+    /*============= preloader js css =============*/
+    var cites = [];
+    cites[0] = "Chúng tôi luôn sẵn sàng được phục vụ bạn.";
+    var cite = cites[Math.floor(Math.random() * cites.length)];
+    $("#preloader p").text(cite);
+    $("#preloader").addClass("loading");
 
-  $(window).on("load", function () {
-    setTimeout(function () {
-      $("#preloader").fadeOut(500, function () {
-        $("#preloader").removeClass("loading");
-      });
-    }, 500);
-  });
+    $(window).on("load", function () {
+        setTimeout(function () {
+            $("#preloader").fadeOut(500, function () {
+                $("#preloader").removeClass("loading");
+            });
+        }, 500);
+    });
 })(jQuery);

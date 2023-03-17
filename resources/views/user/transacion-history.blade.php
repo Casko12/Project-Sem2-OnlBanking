09bcd57c1f74aa21541529364d15c29ce738751b
Casko12 <phamduyhung.ftu@gmail.com>
2023-03-18 01:46:35 +0700

    Online Banking

--- resources/views/user/transacion-history.blade.php
+++ resources/views/user/transacion-history.blade.php
@@ -6,7 +6,7 @@
 
 @section("main_content")
     <div class="col-lg-9">
-        <h2 class="fw-400 mb-3">Transactions</h2>
+        <h2 class="fw-400 mb-3">Lịch sử giao dịch</h2>
 
         <!-- Filter
         ============================================= -->

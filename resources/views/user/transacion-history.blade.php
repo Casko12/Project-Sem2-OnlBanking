e092bb14f13e1f99c05ee3ce8a383e03d211a684
Casko12 <phamduyhung.ftu@gmail.com>
2023-03-18 02:19:14 +0700

    Online Banking

--- resources/views/user/transacion-history.blade.php
+++ resources/views/user/transacion-history.blade.php
@@ -206,16 +206,31 @@
     </div>
 @endsection
 <script>
-    function changeAccount(){
-        var changer = $( "#changerAccount" ).val();
-        $.ajax({
-            url:"/changeAccount/"+"?account_number="+changer,
-            method: "get",
-            success: function (rs) {
-                $("#balance").val(rs.balance)
+    $(function() {
+        'use strict';
+
+        // Date Range Picker
+        $(function() {
+            var start = moment().subtract(29, 'days');
+            var end = moment();
+            function cb(start, end) {
+                $('#dateRange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
             }
-        })
-    }
+            $('#dateRange').daterangepicker({
+                startDate: start,
+                endDate: end,
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                }
+            }, cb);
+            cb(start, end);
+        });
+    });
 </script>
 
 

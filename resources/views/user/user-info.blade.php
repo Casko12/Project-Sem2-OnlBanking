c1e0daf53434d371e428150ac36e93fc6aff9254
Casko12 <phamduyhung.ftu@gmail.com>
2023-03-21 19:22:23 +0700

    Online Banking

--- resources/views/user/user-info.blade.php
+++ resources/views/user/user-info.blade.php
@@ -1,5 +1,16 @@
 @extends("layouts.layout2")
 <base href="{{ url("/") }}"/>
+@section("custom_css")
+    <link rel="stylesheet" type="text/css" href="/user/html/css/bootstrap.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/elegant-icons.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/all.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/animate.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/nice-select.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/default.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/style.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/user/html/css/responsive.css" media="all" />
+    <link href="user/user-page/images/logomba.png" rel="icon" />
+@endsection
 @section("title")
     Trang cá nhân
 @endsection
